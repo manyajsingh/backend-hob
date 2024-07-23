4d4e4c80d29ed33292b58f34b4912c9a18579be3
manyajsingh <139053850+manyajsingh@users.noreply.github.com>
2024-07-23 23:53:33 +0530

    first commit

--- contracts/blog.sol
+++ contracts/blog.sol
@@ -1,27 +1,98 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+// contract Blog {
+//     struct Post {
+//         uint id;
+//         string title;
+//         string content;
+//         address author;
+//     }
+
+//     uint public postCount = 0;
+//     mapping(uint => Post) public posts;
+
+//     event PostCreated(
+//         uint id,
+//         string title,
+//         string content,
+//         address author
+//     );
+
+//     function createPost(string memory _title, string memory _content) public {
+//         postCount++;
+//         posts[postCount] = Post(postCount, _title, _content, msg.sender);
+//         emit PostCreated(postCount, _title, _content, msg.sender);
+//     }
+// }
+
 contract Blog {
-    struct Post {
+
+    event AddTweet(address recipient, uint tweetId);
+    event DeleteTweet(uint tweetId, bool isDeleted);
+
+    struct Tweet {
         uint id;
-        string title;
-        string content;
-        address author;
+        address username;
+        string tweetText;
+        bool isDeleted;
     }
 
-    uint public postCount = 0;
-    mapping(uint => Post) public posts;
+    Tweet[] private tweets;
 
-    event PostCreated(
-        uint id,
-        string title,
-        string content,
-        address author
-    );
+    // Mapping of Tweet id to the wallet address of the user
+    mapping(uint256 => address) tweetToOwner;
 
-    function createPost(string memory _title, string memory _content) public {
-        postCount++;
-        posts[postCount] = Post(postCount, _title, _content, msg.sender);
-        emit PostCreated(postCount, _title, _content, msg.sender);
+    // Method to be called by our frontend when trying to add a new Tweet
+    function addTweet(string memory tweetText, bool isDeleted) external {
+        uint tweetId = tweets.length;
+        tweets.push(Tweet(tweetId, msg.sender, tweetText, isDeleted));
+        tweetToOwner[tweetId] = msg.sender;
+        emit AddTweet(msg.sender, tweetId);
     }
+
+    // Method to get all the Tweets
+    function getAllTweets() external view returns (Tweet[] memory) {
+        Tweet[] memory temporary = new Tweet[](tweets.length);
+        uint counter = 0;
+        for(uint i=0; i<tweets.length; i++) {
+            if(tweets[i].isDeleted == false) {
+                temporary[counter] = tweets[i];
+                counter++;
+            }
+        }
+
+        Tweet[] memory result = new Tweet[](counter);
+        for(uint i=0; i<counter; i++) {
+            result[i] = temporary[i];
+        }
+        return result;
+    }
+
+    // Method to get only your Tweets
+    function getMyTweets() external view returns (Tweet[] memory) {
+        Tweet[] memory temporary = new Tweet[](tweets.length);
+        uint counter = 0;
+        for(uint i=0; i<tweets.length; i++) {
+            if(tweetToOwner[i] == msg.sender && tweets[i].isDeleted == false) {
+                temporary[counter] = tweets[i];
+                counter++;
+            }
+        }
+
+        Tweet[] memory result = new Tweet[](counter);
+        for(uint i=0; i<counter; i++) {
+            result[i] = temporary[i];
+        }
+        return result;
+    }
+
+    // Method to Delete a Tweet
+    function deleteTweet(uint tweetId, bool isDeleted) external {
+        if(tweetToOwner[tweetId] == msg.sender) {
+            tweets[tweetId].isDeleted = isDeleted;
+            emit DeleteTweet(tweetId, isDeleted);
+        }
+    }
+
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,3 +1,17 @@
+// require("@nomicfoundation/hardhat-toolbox");
+// require("dotenv").config();
+
+// /** @type import('hardhat/config').HardhatUserConfig */
+// module.exports = {
+//   solidity: "0.8.24",
+//   networks: {
+//     zkEVMTestnet: {
+//         url: "https://polygonzkevm-cardona.g.alchemy.com/v2/"+process.env.ALCHEMY_API_KEY,
+//         accounts:[process.env.PRIVATE_KEY]
+//     }
+
+//   }
+// };
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
@@ -5,7 +19,7 @@ require("dotenv").config();
 module.exports = {
   solidity: "0.8.24",
   networks: {
-    zkEVMTestnet: {
+    polygonZkEvmTestnet: {
         url: "https://polygonzkevm-cardona.g.alchemy.com/v2/"+process.env.ALCHEMY_API_KEY,
         accounts:[process.env.PRIVATE_KEY]
     }
